25b16610dfe09a1bc6e142b6e29864c2c573d702
Hans Richard <emboh.mail@gmail.com>
2022-01-16 20:29:00 +0700

    init from local

--- src/Components/Home/ServiceBanner.js
+++ src/Components/Home/ServiceBanner.js
@@ -11,19 +11,21 @@ function ServiceBanner() {
   <p className="text-darkgray"> Persiapkan pengajuan beasiswamu sekarang</p>
   </span>
   <div className="flex flex-row items-center justify-center">
+
     <div className="flex-0 ">
-    <img src = {ImagesMentoring} alt="Gambar Layanan Proof Reading" />
-    <span className = "flex flex-col items-center justify-center p-5 gap-3">
-    <h2 className="font-bold"> Proof Reading </h2>
-    <p> Cek Essay, Motivation Letter, dan Reasearch Plan </p>
-    <button className="bg-violet text-lightgray rounded-lg p-3"> Detail Layanan </button>
-    </span>
+      <img src = {ImagesMentoring} alt="Gambar Layanan Proof Reading" />
+      <span className = "flex flex-col items-center justify-center p-5 gap-3">
+        <h2 className="font-bold"> Proof Reading </h2>
+        <p> Cek Essay, Motivation Letter, dan Reasearch Plan </p>
+        <button className="bg-violet text-lightgray rounded-lg p-3"> Detail Layanan </button>
+      </span>
     </div>
+
     <div className="flex-0 flex-col p-7">
       <img src = {ImagesProofReading} alt="Gambar Layanan Intensive mentoring" />
-       <span className = "flex flex-col items-center justify-center p-5 gap-3">
-      <h2 className="font-bold"> Intensive Mentoring </h2>
-      <p> Paket Lengkap Diskusi Bersama Mentor </p>
+      <span className = "flex flex-col items-center justify-center p-5 gap-3">
+        <h2 className="font-bold"> Intensive Mentoring </h2>
+        <p> Paket Lengkap Diskusi Bersama Mentor </p>
       <button className="bg-violet text-lightgray rounded-lg p-3"> Detail Layanan </button>
       </span>
     </div>
